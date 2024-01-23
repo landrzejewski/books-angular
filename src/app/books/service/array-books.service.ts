9e77f8047836421a5675170550ba3968dd490c76
landrzejewski <landrzejewski.poczta@gmail.com>
2024-01-23 15:54:00 +0100

    Rx

--- src/app/books/service/array-books.service.ts
+++ src/app/books/service/array-books.service.ts
@@ -4,45 +4,45 @@ import {BookModel, emptyBookId} from '../models/book.model';
 @Injectable()
 export class ArrayBooksService {
 
-  constructor(@Inject("booksData") @Optional() private books: BookModel[]) {
-    if (!books) {
-      this.books = [];
+    constructor(@Inject("booksData") @Optional() private books: BookModel[]) {
+        if (!books) {
+            this.books = [];
+        }
     }
-  }
 
-  getAll(): BookModel[] {
-    return this.cloneBooks();
-  }
-
-  save(book: BookModel): BookModel {
-    return emptyBookId === book.id ? this.add(book) : this.update(book);
-  }
-
-  private add(book: BookModel): BookModel {
-    const newBook = {...book, id: Date.now()};
-    this.books = [...this.cloneBooks(), newBook];
-    return newBook;
-  }
-
-  private update(book: BookModel): BookModel {
-    const bookIndex = this.getBookIndex(book.id);
-    if (bookIndex !== -1) {
-      const editedBook = {...book};
-      const booksClone = this.cloneBooks();
-      booksClone[bookIndex] = editedBook;
-      this.books = booksClone;
-      return editedBook;
-    } else {
-      throw new Error('Book not found');
+    getAll(): BookModel[] {
+        return this.cloneBooks();
     }
-  }
 
-  private cloneBooks(): BookModel[] {
-    return this.books.map((book) => ({...book}));
-  }
+    save(book: BookModel): BookModel {
+        return emptyBookId === book.id ? this.add(book) : this.update(book);
+    }
 
-  private getBookIndex(id: number): number {
-    return this.books.findIndex((book) => id === book.id);
-  }
+    private add(book: BookModel): BookModel {
+        const newBook = {...book, id: Date.now()};
+        this.books = [...this.cloneBooks(), newBook];
+        return newBook;
+    }
+
+    private update(book: BookModel): BookModel {
+        const bookIndex = this.getBookIndex(book.id);
+        if (bookIndex !== -1) {
+            const editedBook = {...book};
+            const booksClone = this.cloneBooks();
+            booksClone[bookIndex] = editedBook;
+            this.books = booksClone;
+            return editedBook;
+        } else {
+            throw new Error('Book not found');
+        }
+    }
+
+    private cloneBooks(): BookModel[] {
+        return this.books.map((book) => ({...book}));
+    }
+
+    private getBookIndex(id: number): number {
+        return this.books.findIndex((book) => id === book.id);
+    }
 
 }
