ecf2093f3d9c0b7037951ab01144c0150cf0d3d1
landrzejewski <landrzejewski.poczta@gmail.com>
2024-01-23 11:31:24 +0100

    Rx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {BookModel} from './books/models/book.model';
 
 @Component({
   selector: 'app-root',
@@ -7,28 +6,6 @@ import {BookModel} from './books/models/book.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  books: BookModel[]= [
-    {
-      id: 1,
-      title: 'Angular in action',
-      authors: 'Jan Kowalski',
-      category: '#ffbe3c',
-      genre: 'Education',
-      rating: 5,
-      isBestseller: true
-    },
-    {
-      id: 2,
-      title: 'React in action',
-      authors: 'Marek Nowak',
-      category: '#4ba5ff',
-      genre: 'Education',
-      rating: 3,
-      isBestseller: false
-    }
-  ];
-
 }
 
 
