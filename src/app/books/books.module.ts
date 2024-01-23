de10251a717150be98289dcf33eb1e49c0209472
landrzejewski <landrzejewski.poczta@gmail.com>
2024-01-23 11:30:10 +0100

    Rx

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BookCardComponent } from './components/book-card/book-card.component';
-import { BookFormComponent } from './components/book-form/book-form.component';
-import { BooksPageComponent } from './pages/books-page/books-page.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {BookCardComponent} from './components/book-card/book-card.component';
+import {BookFormComponent} from './components/book-form/book-form.component';
+import {BooksPageComponent} from './pages/books-page/books-page.component';
 import {FormsModule} from '@angular/forms';
 import {SharedModule} from '../shared/shared.module';
 import {ArrayBooksService} from './service/array-books.service';
 import books from './models/books.data';
 import {BookModel} from "./models/book.model";
+import {HttpClientModule} from "@angular/common/http";
+import {HttpBooksService} from "./service/http-books.service";
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import {BookModel} from "./models/book.model";
   imports: [
     CommonModule,
     FormsModule,
+    HttpClientModule,
     SharedModule
   ],
   providers: [
@@ -32,7 +35,7 @@ import {BookModel} from "./models/book.model";
     }*/
     {
       provide: 'bookService',
-      useClass: ArrayBooksService
+      useClass: HttpBooksService
     }
   ],
   exports: [
---
+++ src/app/books/service/http-books.service.ts
@@ -0,0 +1,30 @@
+import { Observable } from "rxjs";
+import { BookModel } from "../models/book.model";
+import {BooksService} from "./books.service";
+import {ApiConfiguration} from "../../api.configuration";
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class HttpBooksService implements BooksService {
+
+  constructor(private api: ApiConfiguration, private httpClient: HttpClient) {
+  }
+
+    getAll(): Observable<BookModel[]> {
+        return this.httpClient.get<BookModel[]>(this.api.books);
+    }
+
+    save(book: BookModel): Observable<BookModel> {
+        if (book.id) {
+          return this.httpClient.put<BookModel>(`${this.api.books}/${book.id}`, book);
+        } else {
+          return this.httpClient.post<BookModel>(`${this.api.books}`, book);
+        }
+    }
+
+    filter(property: string, value: string): Observable<BookModel[]> {
+        throw new Error("Method not implemented.");
+    }
+
+}
