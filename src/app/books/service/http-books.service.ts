4c3eb4e77f4c823a7a82086341f568d09a293425
landrzejewski <landrzejewski.poczta@gmail.com>
2024-01-23 16:21:34 +0100

    Rx

--- src/app/books/service/http-books.service.ts
+++ src/app/books/service/http-books.service.ts
@@ -1,5 +1,5 @@
-import { Observable } from "rxjs";
-import { BookModel } from "../models/book.model";
+import {mergeMap, Observable, of} from "rxjs";
+import {BookModel} from "../models/book.model";
 import {BooksService} from "./books.service";
 import {ApiConfiguration} from "../../api.configuration";
 import {HttpClient} from "@angular/common/http";
@@ -8,8 +8,19 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class HttpBooksService implements BooksService {
 
-  constructor(private api: ApiConfiguration, private httpClient: HttpClient) {
-  }
+    constructor(private api: ApiConfiguration, private httpClient: HttpClient) {
+        /*const steram$ = of('http://localhost:3000/ids')
+            .pipe(
+                mergeMap((url) => httpClient.get<number[]>(url)),
+                mergeMap((ids) => ids),
+                mergeMap((id) => httpClient.get<BookModel>(`${this.api.books}/${id}`))
+            );
+
+        steram$
+            .subscribe({
+                next: (value) => console.log(value)
+            });*/
+    }
 
     getAll(): Observable<BookModel[]> {
         return this.httpClient.get<BookModel[]>(this.api.books);
@@ -17,9 +28,9 @@ export class HttpBooksService implements BooksService {
 
     save(book: BookModel): Observable<BookModel> {
         if (book.id) {
-          return this.httpClient.put<BookModel>(`${this.api.books}/${book.id}`, book);
+            return this.httpClient.put<BookModel>(`${this.api.books}/${book.id}`, book);
         } else {
-          return this.httpClient.post<BookModel>(`${this.api.books}`, book);
+            return this.httpClient.post<BookModel>(`${this.api.books}`, book);
         }
     }
 
