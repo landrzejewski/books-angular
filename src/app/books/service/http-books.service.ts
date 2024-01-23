f0141da3d25369cf97480528c3ed0f49cdd49040
landrzejewski <landrzejewski.poczta@gmail.com>
2024-01-23 15:53:13 +0100

    Rx

--- src/app/books/service/http-books.service.ts
+++ src/app/books/service/http-books.service.ts
@@ -1,5 +1,5 @@
-import { Observable } from "rxjs";
-import { BookModel } from "../models/book.model";
+import {Observable} from "rxjs";
+import {BookModel} from "../models/book.model";
 import {BooksService} from "./books.service";
 import {ApiConfiguration} from "../../api.configuration";
 import {HttpClient} from "@angular/common/http";
@@ -11,20 +11,20 @@ export class HttpBooksService implements BooksService {
   constructor(private api: ApiConfiguration, private httpClient: HttpClient) {
   }
 
-    getAll(): Observable<BookModel[]> {
-        return this.httpClient.get<BookModel[]>(this.api.books);
-    }
+  getAll(): Observable<BookModel[]> {
+    return this.httpClient.get<BookModel[]>(this.api.books);
+  }
 
-    save(book: BookModel): Observable<BookModel> {
-        if (book.id) {
-          return this.httpClient.put<BookModel>(`${this.api.books}/${book.id}`, book);
-        } else {
-          return this.httpClient.post<BookModel>(`${this.api.books}`, book);
-        }
+  save(book: BookModel): Observable<BookModel> {
+    if (book.id) {
+      return this.httpClient.put<BookModel>(`${this.api.books}/${book.id}`, book);
+    } else {
+      return this.httpClient.post<BookModel>(`${this.api.books}`, book);
     }
+  }
 
-    filter(property: string, value: string): Observable<BookModel[]> {
-        return this.httpClient.get<BookModel[]>(`${this.api.books}?${property}_like=${value}`);
-    }
+  filter(property: string, value: string): Observable<BookModel[]> {
+    return this.httpClient.get<BookModel[]>(`${this.api.books}?${property}_like=${value}`);
+  }
 
 }
