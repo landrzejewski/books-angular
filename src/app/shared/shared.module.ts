00c4eb7cf8d4d7dcb689d13fdeb4bd24ef4f8213
landrzejewski <landrzejewski.poczta@gmail.com>
2024-01-24 11:23:55 +0100

    Rx

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RatingComponent} from './components/rating/rating.component';
 import {ErrorMessagesComponent} from "./components/error-messages/error-messages.component";
-import {AppEmailValidator} from "./directive/validator/app-email.directive";
+import {AppEmailValidator} from "./directives/validator/app-email.directive";
 
 @NgModule({
   declarations: [
